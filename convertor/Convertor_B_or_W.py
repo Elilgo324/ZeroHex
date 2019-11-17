14638dab413584b35aad4761e1e5892c9fa73ac8
Avshalom Tam <32040834+AvshalomTam@users.noreply.github.com>
2019-11-18 00:30:38 +0200

    Add files via upload

---
+++ convertor/Convertor_B_or_W.py
@@ -0,0 +1,107 @@
+# script gets HEX games in letters like: W[aa];B[bb];W[cd];B[dd])
+# prints the board in every step
+import numpy as np
+filename = "1616DEMO.txt"
+
+def main():
+    c = Convertor(filename, 4)
+    c.convert()
+
+class Convertor:
+    def __init__(self, filename, board_size):
+        self.filename = filename
+        self.board_size = board_size
+
+    def getNumeric(self, letter):
+        return ord(letter) - 97
+
+    def convert(self):
+        class Turn:
+            W = 0
+            B = 1
+
+        # open file of games. read one game
+        with open(filename) as f_in:
+            lines = (line.rstrip() for line in f_in)
+            lines = list(line for line in lines if line)  # Non-blank lines in a list
+
+        # print name of the player
+        name = lines[0]
+        print("Player Name: {}".format(name))
+        # remove first line
+        lines.remove(lines[0])
+
+        file = open("test_file.txt", "w")
+
+        for line in lines:
+            #print(line)
+            # organize locations
+            sp_line = line.split(";")
+            sp_line.remove(sp_line[0])
+
+            Game_info = sp_line[0]
+            # check if ower player is W or B in the specific game
+            W_name = "PW[{}]".format(name)
+            F_or_S = "S"
+            if W_name in Game_info:
+                F_or_S = "F"
+                print("Player is W (first)")
+
+            sp_line.remove(sp_line[0])
+            #print(sp_line)
+            # initialize new board for the game with 0's
+            Wboard = np.zeros((self.board_size, self.board_size))
+            Bboard = np.zeros((self.board_size, self.board_size))
+            board = [Wboard, Bboard]
+
+            locations = []
+            for loc in sp_line:
+                x_val = self.getNumeric(loc[2])
+                y_val = self.getNumeric(loc[3])
+                location = [x_val, y_val]
+                locations.append(location)
+            print(locations)
+
+            turn = Turn.W
+            # modify board
+            index = 0
+            for locat in locations:
+                print("its {} turn:".format(turn))
+                if locat[0] == self.getNumeric("r"):  # when resign
+                    print("user {} left the game".format(turn))
+                    continue
+                elif locat[0] == self.getNumeric("s"):  # when swap
+                    print("user {} swap the game".format(turn))
+                    continue
+
+                # print only player moves
+                if F_or_S == "F":
+                    if turn == Turn.W:
+                        # todo write board to file before move
+                         print(board)
+                         file.write(board.__str__())
+                    board[turn][locat[0], locat[1]] = 1
+                    file.write("\n")
+                    if turn == Turn.W:
+                        # todo write board to file after move
+                         file.write(board.__str__())
+                         file.write("\nnext move:")
+                         print(board)
+                    turn = (turn + 1) % 2
+                else:
+                    if turn == Turn.B:
+                        # todo write board to file before move
+                         file.write(board.__str__())
+                         print(board)
+                    board[turn][locat[0], locat[1]] = 1
+                    file.write("\n")
+                    if turn == Turn.B:
+                        # todo write board to file after move
+                         file.write(board.__str__())
+                         file.write("\nnext move:")
+                         print(board)
+                    turn = (turn + 1) % 2
+            file.write("\nnext game:\n")
+
+if __name__ == "__main__":
+    main()
