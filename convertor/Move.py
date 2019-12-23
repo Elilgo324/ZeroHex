310e3831241a852dc83642da88d4fc51d763e9e4
Avshalom Tam <32040834+AvshalomTam@users.noreply.github.com>
2019-12-23 20:18:46 +0200

    Add files via upload

--- convertor/Move.py
+++ convertor/Move.py
@@ -1,7 +1,5 @@
-class Move(object):
-
-    def __init__(self, color=None, board_stt=None, next_mv=None) -> None:
-        super().__init__()
+class Move:
+    def __init__(self, color=None, board_stt=None, next_mv=None):
         self.color = color
         self.board_stt = board_stt
         self.next_mv = next_mv
