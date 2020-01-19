cf22f3231ed8a6627d3c67d4e7b87c18a5f04797
shlomor25 <shlomor25@gmail.com>
2020-01-19 10:58:10 +0200

    temperature in MTCS

--- agent1_zero_dnn/adam.py
+++ agent1_zero_dnn/adam.py
@@ -1,15 +1,33 @@
-"""
-def update_temp(loss = 1):
+import numpy as np
+
+
+def update_temp(loss=1):
+    """
     deriev loss according params
     deraive 1-0 loss according temp
-
+    :param loss:
+    :return:
+    """
+    num_iterations = 0
+    epsilon = 0
+    beta_1 = 0
+    beta_2 = 0
+    step_size = 0
+    m = 0
+    v = 0
+    w = 0
+    x = 0
+    y = 0
     for t in range(num_iterations):
-        g = compute_gradient(x,y)
+        g = compute_gradient(x, y)
         m = beta_1 * m + (1 - beta_1) * g
-        v = beta_2 * v + (1 - beta_2) * np.power(g,2)
-        m_hat = m / (1 - np.power(beta_1,t))
-        v_hat = v / (1 - np.power(beta_2,t))
+        v = beta_2 * v + (1 - beta_2) * np.power(g, 2)
+        m_hat = m / (1 - np.power(beta_1, t))
+        v_hat = v / (1 - np.power(beta_2, t))
         w = w - step_size * m_hat / (np.sqrt(v_hat) + epsilon)
 
-    return 7
-"""
+    return None
+
+
+def compute_gradient(x, y):
+    return 0
